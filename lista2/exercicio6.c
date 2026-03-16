a068ebaad871d599866f037c29f1244d1b3be2f1
GaMK7 <gabrielmiguelmartins@gmail.com>
2026-03-16 17:44:07 -0400

    Create exercicio6.c

---
+++ lista2/exercicio6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	int lista[4][4];
+	
+	for(int i = 0; i < 4; ++i){
+		for(int j = 0; j < 4; ++j){
+			printf("Digite um numero: ");
+			scanf("%d", &lista[i][j]);
+		}
+	}
+	
+	for(int i = 0; i < 4; ++i){
+		printf("\n");
+		for(int j = 0; j < 4; ++j){
+			printf("[%d] ", lista[i][j]);
+		}
+	}
+	
+	
+	printf("Elementos da diagonal principal da matriz: [%d] [%d] [%d] [%d]", lista[0][3], lista[1][2], lista[2][1], lista[3][0]);
+	
+	return 0;
+}
